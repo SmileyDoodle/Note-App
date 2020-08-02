8c9e8ea0518bd459905f9629306a38e481362ade
SmileyDoodle <smileydoodle.official@gmail.com>
2020-08-02 12:46:30 +0300

    Updates

---
+++ precache-manifest.4c961fbeff0a9ae103c80c699f0f1da9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af04a297eab2168a245e7fb1c99fc2c1",
+    "url": "/Note-App/index.html"
+  },
+  {
+    "revision": "92c4b95b145779e627b9",
+    "url": "/Note-App/static/js/2.d59cb8f0.chunk.js"
+  },
+  {
+    "revision": "6cfaff9748f200eecde2d0a49d419313",
+    "url": "/Note-App/static/js/2.d59cb8f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb257def5f83a7f17ca",
+    "url": "/Note-App/static/js/main.0e78badd.chunk.js"
+  },
+  {
+    "revision": "ec7f917917cf91a679d5",
+    "url": "/Note-App/static/js/runtime-main.727bd211.js"
+  }
+]);
